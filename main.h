aec2a5914d9d4ec31969fb0adb8d70bd8fa3ca9b
TonyGash <tonygaya5@gmail.com>
2022-04-20 01:55:27 +0300

    printf first task

--- main.h
+++ main.h
@@ -1,10 +1,23 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<stdarg.h>
-#include<limits>
+#include <stdarg.h>
+#include <stddef.h>
+void _putchar(char character);
+
+#define sprintf sprintf_
+int sprintf_(char* buffer, const char* format, ...);
+#define snprintf  snprintf_
+#define vsnprintf vsnprintf_
+int  snprintf_(char* buffer, size_t count, const char* format, ...);
+int vsnprintf_(char* buffer, size_t count, const char* format, va_list va);
+#define vprintf vprintf_
+int vprintf_(const char* format, va_list va);
+int fctprintf(void (*out)(char character, void* arg), void* arg, const char* format, ...);
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 #define printf printf_
 int _printf(const char *format, ...);
