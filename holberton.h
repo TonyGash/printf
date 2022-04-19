f8eaf620280dcb5ea170d9fd68a512c3c21c8616
TonyGash <tonygaya5@gmail.com>
2022-04-19 17:57:15 +0300

    printf

---
+++ holberton.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdarg.h>
+#include<limits>
+
+#define printf printf_
+int _printf(const char *format, ...);
+
+#endif // !HOLBERTON_H
