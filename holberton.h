80cda2c5bd184569f8dbb4e68cc59de0f4a02035
TonyGash <tonygaya5@gmail.com>
2022-04-19 18:05:43 +0300

    printf

--- holberton.h
+++ holberton.h
@@ -1,12 +1,33 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<stdarg.h>
-#include<limits>
+#include <stdarg.h>
 
-#define printf printf_
+int _putchar(char c);
+int print_int(va_list arg);
+int print_unsigned(va_list arg);
 int _printf(const char *format, ...);
+int print_char(va_list arg);
+int print_str(va_list arg);
+int print_percent(void);
+void print_binary(unsigned int n, unsigned int* printed);
+int print_unsignedToBinary(va_list arg);
+int print_oct(va_list arg);
+int print_unsignedIntToHex(unsigned int num, char _case);
+int print_hex_base(va_list arg, char _case);
+int print_hex(va_list arg);
+int print_HEX(va_list arg);
+int print_STR (va_list arg);
 
-#endif // !HOLBERTON_H
+/**
+ * struct identifierStruct - structure definition of a printTypeStruct
+ * @indentifier: type
+ * @printer: function to print
+ */
+typedef struct identifierStruct
+{
+char *indentifier;
+int (*printer)(va_list);
+} identifierStruct;
+
+#endif
